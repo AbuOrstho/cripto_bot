890dd520cc09de4b5059b56cd79d6a0388535e64
AbuOrstho <susurkievabdulaziz6@gmail.com>
2024-02-19 08:51:46 +0300

    Сделал калькулятор и добил файл для получения данных

--- calc.py
+++ calc.py
@@ -1,56 +1,16 @@
-import json
+import spread
 
-bitmark_prices = json.load(open(r'BitMart\response_1.json', 'r', encoding="UTF-8"))["data"]["tickers"]
-coinex_prices = json.load(open(r'Coinex\response_1.json', 'r', encoding="UTF-8"))["data"]["ticker"]
-huobi_prices = json.load(open(r'Huobi\response_1.json', 'r', encoding="UTF-8"))["data"]
-lbank_prices = json.load(open(r'LBank\response_1.json', 'r', encoding="UTF-8"))["data"]
-mexc_prices = json.load(open(r'MEXC\response_1.json', 'r', encoding="UTF-8"))["symbols"]
+coin_list = spread.calc()
 
-bitmark_symbols = []
-coinex_symbols = list(coinex_prices.keys())
-huobi_symbols = []
-lbank_symbols = []
-mexc_symbols = []
-
-for i in bitmark_prices:
-    bitmark_symbols.append(i["symbol"])
-
-for i in huobi_prices:
-    huobi_symbols.append(i["symbol"])
-
-for i in lbank_prices:
-    lbank_symbols.append(i["symbol"])
-
-for i in mexc_prices:
-    mexc_symbols.append(i["symbol"])
-
-
-print(mexc_symbols)
-
-f = []
-coin = ['BTC', 'ETH', 'BNB', 'SOL', 'XRP', 'USDC', 'ADA', 'AVAX', 'LINK', 'DOGE', 'TRX', 'DOT', 'MATIC', 'TON', 'ICP',
-        'BCH', 'SHIB', 'DAI', 'LTC', 'IMX', 'UNI', 'LEO', 'ETC', 'ATOM', 'OP', 'NEAR', 'KAS', 'TIA', 'XLM', 'APT',
-        'INJ', 'OKB', 'STX', 'FDUSD', 'FIL', 'LDO', 'HBAR', 'ARB', 'VET', 'XMR', 'CRO', 'MNT', 'SUI', 'MKR', 'RUNE',
-        'RNDR', 'SEI', 'GRT', 'BSV', 'EGLD', 'BEAM', 'ORDI', 'MINA', 'ALGO', 'AAVE', 'HNT', 'QNT', 'FLOW', 'TUSD',
-        'FTM', 'FLR', 'SNX', 'DYM', 'SAND', 'AXS', 'THETA', 'ASTR', 'KCS', 'XTZ', '1000SATS', 'BTT', 'ETHDYDX', 'BGB',
-        'MANA', 'CHZ', 'PYTH', 'NEO', 'EOS', 'BLUR', 'CFX', 'BONK', 'ROSE', 'RON', 'OSMO', 'IOTA', 'WEMIX', 'KLAY',
-        'KAVA', 'PENDLE', 'AKT', 'WOO', 'USDD', 'MANTA', 'GNO', 'ENS', 'FXS', 'LUNC', 'JUP', 'GALA', 'XEC', 'XDC',
-        'AXL', 'CAKE', 'FTT', 'RPL', 'AR', 'CRV', 'NEXO', 'FET', 'APE', 'SC', '1INCH', 'XAUt', 'TWT', 'METIS', 'ZETA',
-        'CORE', 'COMP', 'PEPE', 'GMT', 'BTG', 'GT', 'SUPER', 'LUNA', 'NFT', 'ELF', 'ENJ', 'IOTX', 'XRD', 'SKL', 'GMX',
-        'PAXG', 'CSPR', 'GAS', 'GELO', 'ALT', 'AGIX', 'DESO', 'ZIL', 'KSM', 'WIF', 'USDP', 'ILV', 'BAT', 'UMA', 'MASK',
-        'HOT', 'ZEC', 'ONDO', 'LRC', 'MAGIC', 'SFP', 'NTRN', 'DASH', 'WLD', 'XEM', 'XCH', 'GLMR', 'QTUM', 'API3', 'TRB',
-        'CVX', 'SSV', 'ETHW', 'FLOLI', 'TRAC', 'CFG', 'PYUSD', 'KDA', 'XAI', 'JASMY', 'ANT', 'ID', 'RAY', 'TFUEL', 'JST',
-        'ZRX', 'MX', 'CHR', 'OCEAN', 'SUSHI', 'BAND', 'RVN', 'STORJ', 'T', 'DCR', 'YFI', 'WAVES', 'PRIME', 'ANKR',
-        'GAL', 'MEME', 'BICO', 'RBN', 'CKB', 'JTO', 'OM', 'OAS', 'HT']
-x = 0
-e = []
-for i in coin:
-    x += 1
-    if i + "USDT" in mexc_symbols:
-        f.append(i + "USDT")
+for i in coin_list:
+    spread = ((i[4] - i[2]) / i[2]) * 100
+    if spread > 5:
+        continue
     else:
-        e.append(f"{x} {i + 'USDT'}")
-        print(x+1, f"Пары {i + 'USDT'} нет в mexc")
-
-json_string = json.dumps(f)
-print(len(f), json_string)
+        print(f"Пара {i[0]}\n"
+              f"Минимальная цена: {i[2]} на бирже: {i[1]}\n"
+              f"Максимальная цена: {i[4]} на бирже: {i[3]}\n"
+              f"Спред: {spread}%")
+        print()
+        print()
+        print()
--- spread.py
+++ spread.py
@@ -1,6 +1,9 @@
 import json
 import time
 from collections import Counter
+import time
+
+start_time = time.time()
 
 bitmark_prices = json.load(open(r'BitMart\response_1.json', 'r', encoding="UTF-8"))["data"]["tickers"]
 coinex_prices = json.load(open(r'Coinex\response_1.json', 'r', encoding="UTF-8"))["data"]["ticker"]
@@ -41,41 +44,59 @@ five_lists = [item for item, count in counter.items() if count == 5]
 
 mexc_price = json.load(open(r'MEXC\response_2.json', 'r', encoding="UTF-8"))
 
-# Предполагаем, что five_lists, bitmark_prices, coinex_prices, huobi_prices, lbank_prices, mexc_price уже определены
 
-# Инициализируем словарь для хранения результатов
-coins_prices = {}
+def prices():
+    # Инициализируем словарь для хранения результатов
+    coins_prices = {}
 
-for i in five_lists:
-    # Для каждой монеты создаем словарь с ценами на разных биржах
-    coins_prices[i] = {}
+    for i in five_lists:
+        # Для каждой монеты создаем словарь с ценами на разных биржах
+        coins_prices[i] = {}
 
-    for bp in bitmark_prices:
-        bitmark_coin = bp["symbol"].replace("_", "").upper()
-        if bitmark_coin == i:
-            coins_prices[i]["Bitmark"] = bp["last_price"]
+        for bp in bitmark_prices:
+            bitmark_coin = bp["symbol"].replace("_", "").upper()
+            if bitmark_coin == i:
+                coins_prices[i]["Bitmark"] = float(bp["last_price"])
 
-    for cp in coinex_prices:
-        coinex_coin = cp.replace("_", "").upper()
-        if coinex_coin == i:
-            coins_prices[i]["Coinex"] = coinex_prices[cp]['last']
+        for cp in coinex_prices:
+            coinex_coin = cp.replace("_", "").upper()
+            if coinex_coin == i:
+                coins_prices[i]["Coinex"] = float(coinex_prices[cp]['last'])
 
-    for hp in huobi_prices:
-        huobi_coin = hp["symbol"].replace("_", "").upper()
-        if huobi_coin == i:
-            coins_prices[i]["Huobi"] = hp["close"]
+        for hp in huobi_prices:
+            huobi_coin = hp["symbol"].replace("_", "").upper()
+            if huobi_coin == i:
+                coins_prices[i]["Huobi"] = float(hp["close"])
 
-    for lp in lbank_prices:
-        lbank_coin = lp['symbol'].replace("_", "").upper()
-        if lbank_coin == i:
-            coins_prices[i]["LBank"] = lp["ticker"]["latest"]
+        for lp in lbank_prices:
+            lbank_coin = lp['symbol'].replace("_", "").upper()
+            if lbank_coin == i:
+                coins_prices[i]["LBank"] = float(lp["ticker"]["latest"])
 
-    for mp in mexc_price:
-        mexc_coin = mp["symbol"].replace("_", "").upper()
-        if mexc_coin == i:
-            coins_prices[i]["MEXC"] = mp["price"]
-x = 0
-# Выводим полученный словарь
-for key, value in coins_prices.items():
-    x += 1
-    print(x, key, value.values())
+        for mp in mexc_price:
+            mexc_coin = mp["symbol"].replace("_", "").upper()
+            if mexc_coin == i:
+                coins_prices[i]["MEXC"] = float(mp["price"])
+    print(coins_prices)
+    return coins_prices
+
+
+def calc():
+    all_spread = []
+    # Выводим полученный словарь
+    for keys, values in prices().items():
+        min_b = ""
+        max_b = ""
+        price_list = list(values.values())
+        min_price = min(price_list)
+        max_price = max(price_list)
+        for key, value in values.items():
+            if value == min_price:
+                min_b = key
+            elif value == max_price:
+                max_b = key
+        all_spread.append([keys, min_b, min_price, max_b, max_price])
+    return all_spread
+
+
+print(time.time() - start_time)
